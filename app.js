f67e5c94afd6862132ccb033bba69356e4eced44
macrarazy <alitar085@gmail.com>
2014-08-31 00:42:58 +0500

    Update app.js

--- app.js
+++ app.js
@@ -428,4 +428,6 @@ fs.readFile('./config/ipbans.txt', function (err, data) {
 	Users.checkRangeBanned = Cidr.checker(rangebans);
 });
 
+global.Spamroom = require('./spamroom.js');
+
 reloadCustomAvatars();

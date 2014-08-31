2472df208f00555536e740208a5b94d363942aa4
macrarazy <alitar085@gmail.com>
2014-08-31 16:12:02 +0500

    Update components.js

--- components.js
+++ components.js
@@ -493,45 +493,6 @@ var components = exports.components = {
 		return this.parse('/wall Remember to **vote** even if you don\'t want to battle; that way you\'re still voting for what tier battles you want to watch!');
 	},
 
-	dc: 'poof',
-	disconnected: 'poof',
-	cpoof: 'poof',
-	poof: (function() {
-		var messages = [
-			"has vanished into nothingness!",
-			"used Explosion!",
-			"fell into the void.",
-			"went into a cave without a repel!",
-			"has left the building.",
-			"was forced to give Zarel's mom an oil massage!",
-			"was hit by Magikarp's Revenge!",
-			"ate a bomb!",
-			"is blasting off again!",
-			"(Quit: oh god how did this get here i am not good with computer)",
-			"was unfortunate and didn't get a cool message.",
-			"The Immortal accidently kicked {{user}} from the server!",
-			"{{user}}, BlakJack just fucked you up!"
-		];
-
-		return function(target, room, user) {
-			if (target && !this.can('broadcast')) return false;
-			if (room.id !== 'lobby') return false;
-			var message = target || messages[Math.floor(Math.random() * messages.length)];
-			if (message.indexOf('{{user}}') < 0)
-				message = '{{user}} ' + message;
-			message = message.replace(/{{user}}/g, user.name);
-			if (!this.canTalk(message)) return false;
-
-			var colour = '#' + [1, 1, 1].map(function() {
-				var part = Math.floor(Math.random() * 0xaa);
-				return (part < 0x10 ? '0' : '') + part.toString(16);
-			}).join('');
-
-			room.addRaw('<strong><font color="' + colour + '">~~ ' + Tools.escapeHTML(message) + ' ~~</font></strong>');
-			user.disconnectAll();
-		};
-	})(),
-
 	customsymbol: function(target, room, user) {
 		if (!user.canCustomSymbol) return this.sendReply('You need to buy this item from the shop to use.');
 		if (!target || target.length > 1) return this.parse('/help customsymbol');
